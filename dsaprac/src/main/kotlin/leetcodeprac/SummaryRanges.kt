98966749c23c98156fa178357dcd3ab683e2bb42
Karan Ahuja <karan.ahuja.ext@atomtech.in>
2024-05-06 19:15:03 +0530

    add changes

--- dsaprac/src/main/kotlin/leetcodeprac/SummaryRanges.kt
+++ dsaprac/src/main/kotlin/leetcodeprac/SummaryRanges.kt
@@ -1,4 +1,28 @@
 package leetcodeprac
 //https://leetcode.com/problems/summary-ranges/description/?envType=study-plan-v2&envId=top-interview-150
+fun main() {
+    SummaryRanges().getSummaryRanges(intArrayOf(0,2,3,4,6,8,9))
+}
 class SummaryRanges {
+    fun getSummaryRanges(nums:IntArray){
+        var previous = nums[0]
+        val listOfRanges = mutableListOf<List<Int>>()
+        var currentList = mutableListOf<Int>()
+
+        nums.forEach {
+            if(it == previous || it == previous + 1)
+                currentList.add(it)
+            else{
+                listOfRanges.add(currentList)
+                currentList = mutableListOf<Int>()
+                currentList.add(it)
+
+            }
+            previous = it
+        }
+        listOfRanges.add(currentList)
+        listOfRanges.forEach{
+            println(it)
+        }
+    }
 }
