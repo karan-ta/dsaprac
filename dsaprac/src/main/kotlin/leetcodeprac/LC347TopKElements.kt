b44e6c9c25b9650280c8e33390343f2b7f263842
Karan Ahuja <karan.ahuja.ext@atomtech.in>
2024-05-23 18:37:27 +0530

    add

--- dsaprac/src/main/kotlin/leetcodeprac/LC347TopKElements.kt
+++ dsaprac/src/main/kotlin/leetcodeprac/LC347TopKElements.kt
@@ -4,7 +4,34 @@ LC347TopKElements().topKFrequent(intArrayOf(1,1,1,2,2,3,3),2)
 }
 class LC347TopKElements {
     fun topKFrequent(nums: IntArray, k: Int): IntArray {
+        val inputCounts = hashMapOf<Int,Int>()
+        val countToInputMapArray = arrayOf<MutableList<Int>>()
+        nums.forEach{
+            val currentCount = inputCounts.get(it)?:0
+            inputCounts.put(it,currentCount+1)
 
+        }
+        inputCounts.forEach { t, u ->
+            println(t)
+            println(u)
+            val currentCountList = countToInputMapArray[u]
+            currentCountList.add(t)
+            countToInputMapArray[u] = currentCountList
+        }
+        val outputList = mutableListOf<Int>()
+        var topKAdded = false
+        countToInputMapArray.forEach {
+            if(it.size > 0 && !topKAdded){
+                it.forEach {
+                    outputList.add(it)
+                    if(outputList.size == k)
+                        topKAdded = true
+                }
+
+
+            }
+        }
+        return outputList.toIntArray()
     }
 }
 //49,347,560
